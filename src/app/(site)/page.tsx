67953f28e7c9aedb07af6a5c10f5c81157ec4e87
Victor Mann <victorcmann@gmail.com>
2025-09-10 23:19:09 -0300

    banner com carrossel

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,8 +1,10 @@
+import { Banners } from "@/components/home/banners";
+import { data } from "@/data";
 
 export default function Page() {
   return (
     <div>
-      Opa, tudo bem?
+      <Banners list={data.banners} />
     </div>
   );
 }
