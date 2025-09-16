119dd890d4fa9af03bda352bcf2d7988aef22075
Victor Mann <victorcmann@gmail.com>
2025-09-16 18:52:29 -0300

    hook useQueryString

--- src/components/categories/product-list-filter.tsx
+++ src/components/categories/product-list-filter.tsx
@@ -1,18 +1,32 @@
 "use client"
 
-import { useState } from "react"
+import { useQueryString } from "@/hooks/use-querystring";
+import { ChangeEvent, useState } from "react"
 
 export const ProductListFilter = () => {
+  const queryString = useQueryString();
   const [filterOpened, setFilterOpened] = useState(false);
   
+  const order = queryString.get('order') ?? 'views';
+
+  const handleSelectChanged = (e: ChangeEvent<HTMLSelectElement>) => {
+    queryString.set('order', e.target.value);
+  };
+
 
   return (
     <div>
       <div className="flex flex-col md:flex-row gap-6 justify-between items-start md:items-center">
         <div className="text-3xl"><strong>12</strong> Produtos</div>
         <div className="w-full md:max-w-70 flex flex-row gap-5">
-          <select className="h-14 flex-1 px-6 bg-white border border-gray-200 text-gray-500">
-            <option>Ordenar por</option>
+          <select 
+            defaultValue={order}
+            onChange={handleSelectChanged}
+            className="h-14 flex-1 px-6 bg-white border border-gray-200 text-gray-500"
+          >
+            <option value={'views'}>Popularidade</option>
+            <option value={'price'}>Por preço</option>
+            <option value={'selling'}>Mais vendidos</option>r
           </select>
           <div 
             onClick={() => setFilterOpened(!filterOpened)}
