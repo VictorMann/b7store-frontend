9447acd14e78b0482178a73a13cddd1a401c5ea7
Victor Mann <victorcmann@gmail.com>
2025-09-30 20:39:38 -0300

    Area subtotal do carrinho

--- src/components/cart/cart-container.tsx
+++ src/components/cart/cart-container.tsx
@@ -5,6 +5,8 @@ import { CartListItem } from "@/types/cart-list-item"
 import Image from "next/image";
 import { useEffect } from "react";
 import { CartProductList } from "./cart-product-list";
+import { FinishPurchaseButton } from "./finish-purchase-button";
+import Link from "next/link";
 
 type Props = {
   initialCartProducts: CartListItem[],
@@ -14,6 +16,8 @@ type Props = {
 export const CartContainer = ({ initialCartProducts, initialSubtotal }: Props) => {
   const cartStore = useCartStore(state => state);
   
+  let total = initialSubtotal + cartStore.shippingCost;
+
   useEffect(() => {
     cartStore.clearShipping();
   }, []);
@@ -36,8 +40,37 @@ export const CartContainer = ({ initialCartProducts, initialSubtotal }: Props) =
         <div className="flex-1">
           <CartProductList initialList={initialCartProducts} />
         </div>
-        <div className="flex-1 md:max-w-sm">
-          Info
+        <div className="flex-1 md:max-w-sm flex flex-col gap-4">
+          {/* frete */}
+          <div className="bg-white border border-gray-200 rounded-sm">
+            <div className="border-b border-gray-200 p-6">
+              <div className="flex justify-between items-center mb-5">
+                <div>Subtotal</div>
+                <div className="font-bold">R$ {initialSubtotal.toFixed(2)}</div>
+              </div>
+              <div className="flex justify-between items-center">
+                <div>Frete</div>
+                <div className="font-bold">R$ {cartStore.shippingCost.toFixed(2)}</div>
+              </div>
+            </div>
+            <div className="p-6">
+              <div className="flex justify-between items-center mb-3">
+                <div>Total</div>
+                <div className="font-bold text-2xl text-blue-600">R$ {total.toFixed(2)}</div>
+              </div>
+              <div className="text-right text-xs text-gray-500 mb-5">Em até 12x no cartão.</div>
+
+              <FinishPurchaseButton />
+              <div className="text-center mt-4">
+                <Link 
+                  href={'/'}
+                  className="text-xs text-gray-500 hover:underline">
+                  Comprar outros produtos
+                </Link>
+              </div>
+
+            </div>
+          </div>
         </div>
       </div>
 
---
+++ src/components/cart/finish-purchase-button.tsx
@@ -0,0 +1,15 @@
+export const FinishPurchaseButton = () => {
+  
+  const handleClick = () => {
+
+  };
+  
+  return (
+    <button 
+      onClick={handleClick}
+      type="button"
+      className="cursor-pointer w-full px-6 py-5 bg-blue-600 text-white border-0 rounded-sm hover:opacity-90">
+        Finalizar Compra
+    </button>
+  )
+}
