3ac18384dd229f6ac8ab6ce43f859b67661e7785
Victor Mann <victorcmann@gmail.com>
2025-10-01 16:45:50 -0300

    Botão finalizar compra

--- src/components/cart/finish-purchase-button.tsx
+++ src/components/cart/finish-purchase-button.tsx
@@ -1,14 +1,50 @@
-export const FinishPurchaseButton = () => {
-  
-  const handleClick = () => {
+import { clearCartCookie } from "@/actions/clear-cart-cookie";
+import { finishCart } from "@/actions/finish-cart";
+import { useAuthStore } from "@/store/auth";
+import { useCartStore } from "@/store/cart";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
+export const FinishPurchaseButton = () => {
+  const { token, hydrated } = useAuthStore(state => state);
+  const cartStore = useCartStore(state => state);
+
+  if (!hydrated) return null;
+
+  if (!token) {
+    return (
+      <Link
+        href={'/login'}
+        className="block text-center cursor-pointer w-full px-6 py-5 bg-blue-600 text-white border-0 rounded-sm hover:opacity-90">
+          Faça login para finalizar
+      </Link>
+    )
+  }
+
+  const handleFinishButton = async () => {
+    if (!token || !cartStore.selectedAddressId) return;
+    
+    const sessionUrl = await finishCart(
+      token,
+      cartStore.selectedAddressId,
+      cartStore.cart
+    );
+
+    if (sessionUrl) {
+      await clearCartCookie();
+      cartStore.clearCart();
+      redirect(sessionUrl);
+    } else {
+      alert('Ocorreu um erro');
+    }
   };
   
   return (
     <button 
-      onClick={handleClick}
+      disabled={!cartStore.selectedAddressId ? true : false}
+      onClick={handleFinishButton}
       type="button"
-      className="cursor-pointer w-full px-6 py-5 bg-blue-600 text-white border-0 rounded-sm hover:opacity-90">
+      className="cursor-pointer w-full px-6 py-5 bg-blue-600 text-white border-0 rounded-sm hover:opacity-90 disabled:opacity-20">
         Finalizar Compra
     </button>
   )
